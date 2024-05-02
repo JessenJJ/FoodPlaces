a43a36ca5bdc0558c665e21ef9e4eff31b57ac6c
Jess <jessen2323@gmail.com>
2024-05-02 17:01:44 +0700

    Initial Commit

---
+++ FoodPlaces/FoodPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlacesApp.swift
+//  FoodPlaces
+//
+//  Created by User50 on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
